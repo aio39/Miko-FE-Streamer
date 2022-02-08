d2edd8675d948ef3dac2cb7f6f5e789e3732e33e
Aio <AioDev.JS@gmail.com>
2022-02-08 17:07:24 +0900

    ADD - MetadataList _ Remove

--- src/routes/concert/metadataList/MetadataListView.tsx
+++ src/routes/concert/metadataList/MetadataListView.tsx
@@ -1,13 +1,27 @@
-import { Box, Text, VStack } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Center,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+  VStack,
+} from '@chakra-ui/react';
+import produce from 'immer';
 import { FC } from 'react';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { metadataState } from '../../../recoil/metadataState';
 import {
   MessageMetadata,
   MetaData,
   QuizMetadata,
 } from '../../../types/TimeMetadataFormat';
-
 const MetadataMsgPreview: FC<{ data: MessageMetadata }> = ({ data }) => {
   return (
     <Box width="full" h="100" border="1px">
@@ -21,6 +35,65 @@ const MetadataQuizPreview: FC<{ data: QuizMetadata }> = ({ data }) => {
   return (
     <Box width="full" h="100" border="1px">
       <Text>{data.mainText}</Text>
+    </Box>
+  );
+};
+
+const MetadataPreviewContainer: FC<{ data: MetaData }> = ({
+  children,
+  data,
+}) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const setMetadata = useSetRecoilState(metadataState);
+
+  const handleRemoveMetadata = () => {
+    setMetadata((prev) =>
+      produce(prev, (draft) => {
+        const idx = draft.findIndex((d) => d.createdAt === data.createdAt);
+        draft.splice(idx, 1);
+        return draft;
+      })
+    );
+  };
+
+  return (
+    <Box width="full" h="100" border="1px" position="relative">
+      <Text>{children}</Text>
+      <Box
+        position="absolute"
+        right="0"
+        bottom="0"
+        w="10"
+        h="10"
+        bg="red"
+        onClick={handleRemoveMetadata}
+      ></Box>
+      <Center
+        position="absolute"
+        right="16"
+        bottom="0"
+        w="10"
+        h="10"
+        bg="blue"
+        onClick={onOpen}
+      ></Center>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>데이터 전송</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>예약하기</Text>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 };
@@ -45,7 +118,13 @@ const MetadataListContainer = () => {
     <Box w="full">
       <Text>리스트</Text>
       <VStack>
-        {metadata.map((data, idx) => metadataDrawSwitch(data, idx))}
+        {metadata.map((data, idx) => {
+          return (
+            <MetadataPreviewContainer data={data}>
+              {metadataDrawSwitch(data, idx)}
+            </MetadataPreviewContainer>
+          );
+        })}
       </VStack>
     </Box>
   );
---
+++ src/routes/concert/metadataList/metadataPopup/MetadataPopup.tsx
@@ -0,0 +1,7 @@
+import { Box } from '@chakra-ui/react';
+
+const MetadataPopup = () => {
+  return <Box></Box>;
+};
+
+export default MetadataPopup;
