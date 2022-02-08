eb7f826d8d4a78ffc9f426b4808e7e7408aa969d
Aio <AioDev.JS@gmail.com>
2022-02-08 15:30:41 +0900

    ADD - CreateMsg _ Preview

--- src/routes/concert/createMessage/CreateMessage.tsx
+++ src/routes/concert/createMessage/CreateMessage.tsx
@@ -1,13 +1,11 @@
 import { Box, HStack } from '@chakra-ui/react';
-import { useRecoilValue } from 'recoil';
 import Screen169 from '../../../components/hoc/Screen169';
 import {
-  draftMsgBoxDataState,
   draftMsgMainTextState,
-  draftMsgPositionState,
   draftMsgSubTextState,
 } from '../../../recoil/draftMessageState';
 import CreateBtn from './CreateBtn';
+import DraftMessagePreview from './DraftMessagePreview';
 import DragTest from './DragTest';
 import LinkInput from './LinkInput';
 import MsgBoxEditor from './MsgBoxEditor';
@@ -15,112 +13,12 @@ import MsgResetBtn from './MsgResetBtn';
 import MsgTextDataEditor from './MsgTextDataEditor';
 import { PositionSelector } from './PositionSelector';
 
-// const EditableText: FC<{ type: 'mt' | 'st' }> = ({ type }) => {
-//   const draftMessage = useRecoilValue(draftMsgState);
-
-//   function EditableControls() {
-//     const {
-//       isEditing,
-//       getSubmitButtonProps,
-//       getCancelButtonProps,
-//       getEditButtonProps,
-//     } = useEditableControls();
-
-//     return (
-//       <Box position="absolute" right="-10px" transform="translate(100%,0)">
-//         {isEditing ? (
-//           <ButtonGroup justifyContent="center" size="sm">
-//             <IconButton
-//               icon={<CheckIcon />}
-//               {...getSubmitButtonProps()}
-//               aria-label="a"
-//             />
-//             <IconButton
-//               icon={<CloseIcon />}
-//               {...getCancelButtonProps()}
-//               aria-label="a"
-//             />
-//           </ButtonGroup>
-//         ) : (
-//           <Flex justifyContent="center">
-//             <IconButton
-//               size="sm"
-//               icon={<EditIcon />}
-//               {...getEditButtonProps()}
-//               aria-label="a"
-//             />
-//           </Flex>
-//         )}
-//       </Box>
-//     );
-//   }
-
-//   const handleOnSubmit = (text: string) => {
-//     switch (type) {
-//       case 'mt':
-//         setDraftMessage((pre) => ({ ...pre, mt: text }));
-//         break;
-//       case 'st':
-//         setDraftMessage((pre) => ({ ...pre, st: text }));
-//         break;
-//       default:
-//         break;
-//     }
-//   };
-
-//   return (
-//     <Editable
-//       //   textAlign="start"
-//       defaultValue={type === 'mt' ? draftMessage.mt : draftMessage.st}
-//       fontSize="2xl"
-//       isPreviewFocusable={false}
-//       width="auto"
-//       display="flex"
-//       position="relative"
-//       onSubmit={handleOnSubmit}
-//       placeholder={type === 'mt' ? '메인 텍스트' : '서브 텍스트'}
-//       color={type === 'mt' ? draftMessage.mtc : draftMessage.stc}
-//     >
-//       <EditablePreview />
-//       <EditableInput width="auto" />
-//       <EditableControls />
-//     </Editable>
-//   );
-// };
-
-const positionSwith = {
-  1: { left: 0, top: 0 },
-  2: { left: '50%', top: 0, transform: 'translate(-50%,0)' },
-  3: { right: 0, top: 0 },
-  4: { left: 0, top: '50%', transform: 'translate(0,-50%)' },
-  5: { left: '50%', top: '50%', transform: 'translate(-50%,-50%)' },
-  6: { right: 0, top: '50%', transform: 'translate(0,-50%)' },
-  7: { left: 0, bottom: 0 },
-  8: { left: '50%', bottom: 0, transform: 'translate(-50%,0)' },
-  9: { right: 0, bottom: 0 },
-};
-
-const VideoMessage = () => {
-  const positionIndex = useRecoilValue(draftMsgPositionState);
-  const boxData = useRecoilValue(draftMsgBoxDataState);
-  return (
-    <Box
-      position="absolute"
-      {...positionSwith[positionIndex]}
-      bgColor={boxData?.hexColor}
-    >
-      {/* <EditableText type="mt" />
-      <EditableText type="st" /> */}
-    </Box>
-  );
-};
-
 const CreateMsg = () => {
   return (
     <Box overflowY="scroll" h="full">
       <Box>
         <Screen169>
-          <VideoMessage />
+          <DraftMessagePreview />
         </Screen169>
       </Box>
       <HStack>
---
+++ src/routes/concert/createMessage/DraftMessagePreview.tsx
@@ -0,0 +1,124 @@
+import { Box, Text } from '@chakra-ui/react';
+import { FC } from 'react';
+import { useRecoilValue } from 'recoil';
+import { draftMsgState } from '../../../recoil/draftMessageState';
+import { MsgTextData } from '../../../types/TimeMetadataFormat';
+
+// const EditableText: FC<{ type: 'mt' | 'st' }> = ({ type }) => {
+//   const draftMessage = useRecoilValue(draftMsgState);
+
+//   function EditableControls() {
+//     const {
+//       isEditing,
+//       getSubmitButtonProps,
+//       getCancelButtonProps,
+//       getEditButtonProps,
+//     } = useEditableControls();
+
+//     return (
+//       <Box position="absolute" right="-10px" transform="translate(100%,0)">
+//         {isEditing ? (
+//           <ButtonGroup justifyContent="center" size="sm">
+//             <IconButton
+//               icon={<CheckIcon />}
+//               {...getSubmitButtonProps()}
+//               aria-label="a"
+//             />
+//             <IconButton
+//               icon={<CloseIcon />}
+//               {...getCancelButtonProps()}
+//               aria-label="a"
+//             />
+//           </ButtonGroup>
+//         ) : (
+//           <Flex justifyContent="center">
+//             <IconButton
+//               size="sm"
+//               icon={<EditIcon />}
+//               {...getEditButtonProps()}
+//               aria-label="a"
+//             />
+//           </Flex>
+//         )}
+//       </Box>
+//     );
+//   }
+
+//   const handleOnSubmit = (text: string) => {
+//     switch (type) {
+//       case 'mt':
+//         setDraftMessage((pre) => ({ ...pre, mt: text }));
+//         break;
+//       case 'st':
+//         setDraftMessage((pre) => ({ ...pre, st: text }));
+//         break;
+//       default:
+//         break;
+//     }
+//   };
+
+//   return (
+//     <Editable
+//       //   textAlign="start"
+//       defaultValue={type === 'mt' ? draftMessage.mt : draftMessage.st}
+//       fontSize="2xl"
+//       isPreviewFocusable={false}
+//       width="auto"
+//       display="flex"
+//       position="relative"
+//       onSubmit={handleOnSubmit}
+//       placeholder={type === 'mt' ? '메인 텍스트' : '서브 텍스트'}
+//       color={type === 'mt' ? draftMessage.mtc : draftMessage.stc}
+//     >
+//       <EditablePreview />
+//       <EditableInput width="auto" />
+//       <EditableControls />
+//     </Editable>
+//   );
+// };
+
+const positionSwith = {
+  1: { left: 0, top: 0 },
+  2: { left: '50%', top: 0, transform: 'translate(-50%,0)' },
+  3: { right: 0, top: 0 },
+  4: { left: 0, top: '50%', transform: 'translate(0,-50%)' },
+  5: { left: '50%', top: '50%', transform: 'translate(-50%,-50%)' },
+  6: { right: 0, top: '50%', transform: 'translate(0,-50%)' },
+  7: { left: 0, bottom: 0 },
+  8: { left: '50%', bottom: 0, transform: 'translate(-50%,0)' },
+  9: { right: 0, bottom: 0 },
+};
+
+const MessageText: FC<{ data: MsgTextData }> = ({ data }) => {
+  const { text, bold, font, size, hexColor } = data;
+  return (
+    <Text fontSize={size} fontFamily={font} bold={bold} color={hexColor}>
+      {text}
+    </Text>
+  );
+};
+
+const DraftMessagePreview = () => {
+  //   const positionIndex = useRecoilValue(draftMsgPositionState);
+  //   const boxData = useRecoilValue(draftMsgBoxDataState);
+  const draftMsgData = useRecoilValue(draftMsgState);
+  const { boxData, mainTextData, subTextData, positionIndex } = draftMsgData;
+  return (
+    <Box
+      position="absolute"
+      {...positionSwith[positionIndex]}
+      bgColor={boxData?.hexColor}
+      width={boxData.width}
+      height={boxData.height}
+      borderRadius={boxData.round}
+    >
+      {/* <EditableText type="mt" />
+      <EditableText type="st" /> */}
+      <MessageText data={mainTextData} />
+      <Box height={boxData.spacing + 'px'}></Box>
+      <MessageText data={subTextData} />
+    </Box>
+  );
+};
+
+export default DraftMessagePreview;
