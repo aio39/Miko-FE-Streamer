6c98eb20c98d82e4e052b48de0c15a7743be18a8
Aio <AioDev.JS@gmail.com>
2022-02-11 14:04:36 +0900

    ADD - SideBar Icon

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,10 @@
-export const Message = 'message'
-export const Quiz = 'quiz'
+export const Message = 'message';
+export const Quiz = 'quiz';
+export const Ranking = 'ranking';
+export const Information = 'information';
 
-export type  WindowType = typeof Message |  typeof Quiz
+export type WindowType =
+  | typeof Message
+  | typeof Quiz
+  | typeof Information
+  | typeof Ranking;
--- src/routes/concert/sideBar/SideBar.tsx
+++ src/routes/concert/sideBar/SideBar.tsx
@@ -1,38 +1,53 @@
-import { Center, VStack } from "@chakra-ui/react";
-import { FC } from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { Message, Quiz, WindowType } from "../../../const";
-import { selectedWindowState } from "../../../recoil/selectedWindowState";
+import { Center, VStack } from '@chakra-ui/react';
+import { FC } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import {
+  Information,
+  Message,
+  Quiz,
+  Ranking,
+  WindowType,
+} from '../../../const';
+import { selectedWindowState } from '../../../recoil/selectedWindowState';
+import { ReactComponent as InformationLogo } from './../../../svg/information.svg';
 import { ReactComponent as MessageLogo } from './../../../svg/message.svg';
 import { ReactComponent as QuizLogo } from './../../../svg/quiz.svg';
+import { ReactComponent as RankingLogo } from './../../../svg/ranking.svg';
 
+const IconWrapper: FC<{ type: WindowType }> = ({ type, children }) => {
+  const [selectedWindow, setSelectedWindow] =
+    useRecoilState(selectedWindowState);
 
+  const handleChangeWindow = () => {
+    setSelectedWindow(type);
+  };
 
-const IconWrapper: FC<{type: WindowType}> = ({type,children}) => {
-    const [selectedWindow, setSelectedWindow] = useRecoilState(selectedWindowState);
-
-    const handleChangeWindow = () => {
-        setSelectedWindow(type)
-    }
-
-    return <Center w="full"  h="full"  onClick={handleChangeWindow} >
-        {children}
+  return (
+    <Center w="full" h="full" onClick={handleChangeWindow}>
+      {children}
     </Center>
-}
-
+  );
+};
 
 const SideBar = () => {
+  const selectedWindow = useRecoilValue(selectedWindowState);
 
-    const  selectedWindow = useRecoilValue(selectedWindowState)
+  return (
+    <VStack pt="2" px="2">
+      <IconWrapper type={Quiz}>
+        <QuizLogo />
+      </IconWrapper>
+      <IconWrapper type={Message}>
+        <MessageLogo />
+      </IconWrapper>
+      <IconWrapper type={Information}>
+        <InformationLogo />
+      </IconWrapper>
+      <IconWrapper type={Ranking}>
+        <RankingLogo />
+      </IconWrapper>
+    </VStack>
+  );
+};
 
-    return <VStack pt="2" px="2"  > 
-       <IconWrapper type={Quiz}>
-            <QuizLogo   />
-       </IconWrapper>
-       <IconWrapper type={Message} >
-            <MessageLogo />
-       </IconWrapper>
-    </VStack>   
-}
-
-export default SideBar
+export default SideBar;
