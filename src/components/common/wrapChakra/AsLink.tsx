0c82ae1dacf1f361cf5ea059a2ce2421c5176bc8
Aio <AioDev.JS@gmail.com>
2022-02-24 18:12:58 +0900

    ADD - Link to Concert List

---
+++ src/components/common/wrapChakra/AsLink.tsx
@@ -0,0 +1,16 @@
+import { Link } from '@chakra-ui/react';
+import { FC } from 'react';
+import { Link as ReachLink } from 'react-router-dom';
+
+const AsLink: FC<React.ComponentProps<typeof Link>> = ({
+  children,
+  ...props
+}) => {
+  return (
+    <Link as={ReachLink} {...props}>
+      {children}
+    </Link>
+  );
+};
+
+export default AsLink;
--- src/pages/my/ConcertListPage.tsx
+++ src/pages/my/ConcertListPage.tsx
@@ -8,6 +8,7 @@ import {
   useColorModeValue,
   VStack,
 } from '@chakra-ui/react';
+import AsLink from 'components/common/wrapChakra/AsLink';
 import { S3_URL } from 'const';
 import convertDate from 'helper/convertDate';
 import React, { FC } from 'react';
@@ -21,74 +22,76 @@ const ConcertCard: FC<{ data: Concert }> = ({ data }) => {
   const endDate = convertDate(data.all_concert_end_date, 'YMDHM');
 
   return (
-    <Box
-      w="sm"
-      // mx="auto"
-      bg={useColorModeValue('white', 'gray.800')}
-      shadow="lg"
-      rounded="lg"
-      overflow="hidden"
-    >
-      <Image
-        w="full"
-        h={56}
-        fit="cover"
-        objectPosition="center"
-        src={S3_URL + data.cover_image}
-        alt="avatar"
-      />
+    <AsLink to={`/my/concerts/${data.id}`}>
+      <Box
+        w="sm"
+        // mx="auto"
+        bg={useColorModeValue('white', 'gray.800')}
+        shadow="lg"
+        rounded="lg"
+        overflow="hidden"
+      >
+        <Image
+          w="full"
+          h={56}
+          fit="cover"
+          objectPosition="center"
+          src={S3_URL + data.cover_image}
+          alt="avatar"
+        />
 
-      <Flex alignItems="center" px={6} py={3} bg="gray.900">
-        <Icon as={MdSchedule} h={6} w={6} color="white" />
+        <Flex alignItems="center" px={6} py={3} bg="gray.900">
+          <Icon as={MdSchedule} h={6} w={6} color="white" />
 
-        <VStack flexGrow="1">
-          <chakra.h3
-            mx={3}
-            color="white"
-            fontWeight="bold"
-            fontSize="md"
-            alignSelf="start"
-          >
-            {startDate} ~
-          </chakra.h3>
-          <chakra.h3
-            mx={3}
-            color="white"
-            fontWeight="bold"
-            fontSize="md"
-            alignSelf="end"
-          >
-            {endDate}
-          </chakra.h3>
-        </VStack>
-      </Flex>
-
-      <Box py={4} px={6}>
-        <chakra.h1
-          fontSize="xl"
-          fontWeight="bold"
-          color={useColorModeValue('gray.800', 'white')}
-        >
-          {data.title}
-        </chakra.h1>
-
-        <chakra.p py={2} color={useColorModeValue('gray.700', 'gray.400')}>
-          {data.detail}
-        </chakra.p>
-
-        <Flex
-          alignItems="center"
-          mt={4}
-          color={useColorModeValue('gray.700', 'gray.200')}
-        >
-          <Icon as={MdMusicNote} h={6} w={6} mr={2} />
-
-          <chakra.h1 px={2} fontSize="sm">
-            {data.artist}
-          </chakra.h1>
+          <VStack flexGrow="1">
+            <chakra.h3
+              mx={3}
+              color="white"
+              fontWeight="bold"
+              fontSize="md"
+              alignSelf="start"
+            >
+              {startDate} ~
+            </chakra.h3>
+            <chakra.h3
+              mx={3}
+              color="white"
+              fontWeight="bold"
+              fontSize="md"
+              alignSelf="end"
+            >
+              {endDate}
+            </chakra.h3>
+          </VStack>
         </Flex>
+
+        <Box py={4} px={6}>
+          <chakra.h1
+            fontSize="xl"
+            fontWeight="bold"
+            color={useColorModeValue('gray.800', 'white')}
+          >
+            {data.title}
+          </chakra.h1>
+
+          <chakra.p py={2} color={useColorModeValue('gray.700', 'gray.400')}>
+            {data.detail}
+          </chakra.p>
+
+          <Flex
+            alignItems="center"
+            mt={4}
+            color={useColorModeValue('gray.700', 'gray.200')}
+          >
+            <Icon as={MdMusicNote} h={6} w={6} mr={2} />
+
+            <chakra.h1 px={2} fontSize="sm">
+              {data.artist}
+            </chakra.h1>
+          </Flex>
+        </Box>
       </Box>
-    </Box>
+    </AsLink>
   );
 };
 
